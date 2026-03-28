538f657d1b632058dd83da484244e8b5e65d238d
hijunglit <hijunglit@gmail.com>
2026-03-28 10:08:21 +0900

    webtoon app

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,11 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFFE7626C),
-          surface: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
       home: HomeScreen(),
     );
   }
