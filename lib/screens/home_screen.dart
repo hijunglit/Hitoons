4f68ba0fab3607df67c1a555b1825434cb8020f9
hijunglit <hijunglit@gmail.com>
2026-03-31 17:54:12 +0900

    FutureBuilder(...)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hitoons/models/webtoon_model.dart';
+import 'package:hitoons/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +15,21 @@ class HomeScreen extends StatelessWidget {
         elevation: 2,
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
-        title: Text(
+        title: const Text(
           "오늘의 웹툰",
-          style: TextStyle(fontSize: 24),
+          style: TextStyle(
+            fontSize: 24,
+          ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is data");
+          }
+          return const Text("Loading...");
+        },
       ),
     );
   }
